3794c1c332e765e9521ee18dccd99d6579ebe9bd
Kennung Wintersemester Acc158 <w158@atcremers49.informatik.tu-muenchen.de>
2015-12-02 17:59:56 +0100

    1st ex completed.

--- vmcv_ex04/main_ex4.m
+++ vmcv_ex04/main_ex4.m
@@ -12,10 +12,22 @@ SNR = 10;
 f = addnoise(u,SNR,u);
 figure(1), subplot(1,3,2), imshow(f),title(sprintf('noisy %d dB',SNR))
 
-%
-n = numel(u);
-lambda = 1; % inverse regularization parameter
+%% ex4.2 denoising with L2 regularization
+% build up the linear denoising eqn lambda*(u-f) - laplacian*u = 0
+% equivalently solving the linear eqn ->  (lambda - laplacian)*u = lambda*f
+% def matrix A = (lambda - laplacian)
+n = numel(f);
+lambda = 1e0; % inverse regularization parameter
 bdy = 'Neumann';
-fgradMtx = grad_mtx(f, bdy);
-flaplacianMtx = -fgradMtx'*fgradMtx;
-A = lambda*speye(n) - laplacianMtx;
+fgradMtx = grad_mtx(f, bdy);    % gradient operator corresponds dim of f
+flaplacianMtx = -fgradMtx'*fgradMtx; % laplacian operator corresponds dim of f
+A = lambda*speye(n) - flaplacianMtx; % matrix A of the linear eqn above.
+
+u_denoised_l2_vec = A\(lambda*vec(f));
+u_denoised_l2 = reshape(u_denoised_l2_vec, size(f));
+figure(1), subplot(1,3,3), imshow(u_denoised_l2),title('denoised version - L2')
+
+%% ex4.3 denoising with L1 regularization 
+% Rudin-Osher-Fatemi(ROF) functional
+
+
