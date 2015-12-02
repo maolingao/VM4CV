1f9ebe426e11b70decb83ef98ac05f2b8c05d75e
Kennung Wintersemester Acc158 <w158@atcremers49.informatik.tu-muenchen.de>
2015-12-02 18:56:50 +0100

    ex2 ROF not denoise.

--- vmcv_ex04/main_ex4.m
+++ vmcv_ex04/main_ex4.m
@@ -5,12 +5,12 @@
 u = double(imread('lena.pgm'));
 xsize = size(u);
 u = regular(u); 
-figure(1), subplot(1,3,1), imshow(u),title('ground truth')
+figure(1), subplot(2,2,1), imshow(u),title('ground truth')
 
 % add noise
 SNR = 10;
 f = addnoise(u,SNR,u);
-figure(1), subplot(1,3,2), imshow(f),title(sprintf('noisy %d dB',SNR))
+figure(1), subplot(2,2,2), imshow(f),title(sprintf('noisy %d dB',SNR))
 
 %% ex4.2 denoising with L2 regularization
 % build up the linear denoising eqn lambda*(u-f) - laplacian*u = 0
@@ -25,24 +25,29 @@ A = lambda*speye(n) - flaplacianMtx; % matrix A of the linear eqn above.
 
 u_denoised_l2_vec = A\(lambda*vec(f));
 u_denoised_l2 = reshape(u_denoised_l2_vec, size(f));
-figure(1), subplot(1,3,3), imshow(u_denoised_l2),title('denoised version - L2')
+figure(1), subplot(2,2,3), imshow(u_denoised_l2),title('denoised version - L2')
 
 %% ex4.3 denoising with L1 regularization 
 % Rudin-Osher-Fatemi(ROF) functional
 % solver - gradient descent
 % cancellation criteria
-aux.itr = 1e5;          % iteration limit
-aux.tol = 1e-6;         % smallest update of estimate
+aux.itr = 1e2;          % iteration limit
+aux.tol = 1e-16;         % smallest update of estimate
 epsilon = 1e-6;         % for numerical stable
 div = -fgradMtx';       % div = - \nabla^T
 
 % gradient descent solver
 x0 = f;         % initial guess
 fun = @(x)(lambda/2*norm((x-f),'fro')^2 + sum(vec(abs(select(grad(x),3)))));    % loss
-d_grad = @(x)(lambda*(x-f) - div*(fgradMtx*x./(abs(fgradMtx*x)+epsilon)));                       % gradient of loss
+d_grad = @(x)(lambda*(f-f) - reshape(div*(fgradMtx*vec(f)./(abs(fgradMtx*vec(f))+epsilon)),size(f)));                       % gradient of loss
 
-x_gd = gd(fun,d_grad,x0,aux);                     % gradient descent solver
+u_denoised_l1 = gd(fun,d_grad,x0,aux);                     % gradient descent solver
+
+
+figure(1), subplot(2,2,4), imshow(u_denoised_l1),title('denoised version - L1(ROF)')
 
 % check sol 
-diff = norm(x_gd - u)
+% diff_l1 = norm(u_denoised_l1 - u)
+% diff_l2 = norm(u_denoised_l2 - u)
+
 
